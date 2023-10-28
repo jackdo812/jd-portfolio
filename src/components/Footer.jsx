33624e3d17b44bff7036cbdbbe1fd978bcb8bc0d
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-10-27 22:59:18 -0700

    added ThumbNav successfully

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { getYear } from "../utilities/dates";
+import ThumbNav from './ThumbNav';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className='pb-16'>
         <p>&copy;{getYear()} by Jack Do</p>
+        <ThumbNav />
     </footer>
   )
 }
