2c3315c15b60d6a68fa0045dd5d92fcf80b287ff
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-10-28 00:37:34 -0700

    added basic timeline feature without content and styling

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -4,6 +4,12 @@ import Loading from '../components/Loading'
 // TanQuery Components
 import {useQuery} from '@tanstack/react-query';
 import {getPage} from '../api/fetchData';
+import { MdWork } from "react-icons/md";
+
+
+// Timeline components
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 const Experiences = () => {
 
@@ -23,6 +29,70 @@ const Experiences = () => {
                 <h1>{data.title.rendered}</h1>
                 <div className="entry-content" dangerouslySetInnerHTML={{__html:data.content.rendered}}>
                 </div>
+                {/* Timeline section */}
+                <section className='timeline-section'>
+                <VerticalTimeline lineColor="green">
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        date="2011 - present"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', border: '2px solid green' }}
+                        // iconClassName='border-2 border-green-600'
+                        icon={<MdWork />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                        <p>
+                        Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                        </p>
+                        <button className='bg-red-600'>View More</button>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        date="2010 - 2011"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        icon={<MdWork />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Art Director</h3>
+                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                        <p>
+                        Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        date="2008 - 2010"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        icon={<MdWork />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                        <p>
+                        User Experience, Visual Design
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        date="2006 - 2008"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        icon={<MdWork />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                        <p>
+                        User Experience, Visual Design
+                        </p>
+                    </VerticalTimelineElement>
+                    
+                    </VerticalTimeline>
+                </section>
             </article>
       
         </>
