5183028811ba8291568b51cd479b8e07f5edec6f
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-10-27 23:46:32 -0700

    added Menu/Close toggle Menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Nav from './Nav'
+import { useState } from 'react'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
   return (
     <header id="masthead" className="site-header">
         <div className="site-branding">
         <p className="site-title">JD Portfolio</p>
         </div>
+        <div className='hamburger-menu'>
+          <button onClick={toggleMenu}>{isMenuOpen ? 'Close' : 'Menu'}</button>
+        </div>
+        {isMenuOpen &&
         <Nav />
+        } 
   </header>
   )
 }
