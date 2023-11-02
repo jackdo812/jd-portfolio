139921fa853b072775bc6379a7460e5a7ff065a1
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-02 15:39:24 -0700

    added link to Home to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Nav from './Nav'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 
 const Header = ({mediaData, connectData}) => {
@@ -18,7 +19,9 @@ const Header = ({mediaData, connectData}) => {
   return (
     <header id="masthead" className="site-header relative">
         <div className='flex items-center'>
-          <img className='w-[70px] m-2 ' src={mediaData.logo} alt="Logo" />
+          <Link to='/'>
+            <img className='w-[70px] m-2 ' src={mediaData.logo} alt="Logo" />
+          </Link>
           <div className='hamburger-menu hidden sm:block sm:before:content-[""] sm:before:inline-block sm:before:w-[2px] sm:before:h-[26px] sm:before:mr-[1rem] sm:before:bg-black sm:before:align-middle'>
             <button onClick={toggleMenu} className={`text-blue-500 scale-1 hover:scale-x-[1.15] transition-all duration-300 ease-in-out ${isMenuOpen ? 'animate-fade' : 'animate-fade2'}`}>{isMenuOpen ? 'Close': 'Menu'}</button>
     
