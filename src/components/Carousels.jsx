f72868c68cfacae9beb654d2fa6fe6e93d4242b4
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-02 01:07:05 -0700

    added links to single project at See other project section

--- src/components/Carousels.jsx
+++ src/components/Carousels.jsx
@@ -2,6 +2,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NextIconSvg, PrevIconSvg } from '../assets/images/NextPrevIcon';
 import Loading from './Loading';
+import { Link } from 'react-router-dom';
 // TanQuery Components
 import {useQuery} from '@tanstack/react-query';
 import {getPage, getPost} from '../api/fetchData';
@@ -28,12 +29,14 @@ function Carousels({singleProjectData}) {
           <Carousel.Item key={index}>
             <Carousel.Caption className='relative right-0 bottom-0 left-0 text-red-600 w-[75%] mx-auto my-0'>
               <article className='border border-black rounded pb-4'>
-                <img className='w-[300px] my-0 mx-auto' src={post.acf.video_teaser} alt="Project's GIF teaser" />
-                <h3 className='font-bold'>{post.title.rendered}</h3>
-                <p>{post._embedded["wp:term"][1].map((tag, idx) => (
-                  <span key={idx}>{tag.name}{idx < post._embedded["wp:term"][1].length - 1 ? " | " : null}</span>
-                ))}
-                </p>
+                <Link to={`/projects/${post.slug}`}>
+                  <img className='w-[300px] my-0 mx-auto' src={post.acf.video_teaser} alt="Project's GIF teaser" />
+                  <h3 className='font-bold'>{post.title.rendered}</h3>
+                  <p>{post._embedded["wp:term"][1].map((tag, idx) => (
+                    <span key={idx}>{tag.name}{idx < post._embedded["wp:term"][1].length - 1 ? " | " : null}</span>
+                  ))}
+                  </p>
+                </Link>
               </article>
             </Carousel.Caption>
           </Carousel.Item>
