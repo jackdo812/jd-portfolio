b03d752f38f993749b16f303b0d31a302f006dd3
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-09 14:34:30 -0800

    added padding to NavLink

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,23 +15,23 @@ const Nav = () => {
       <ul className="font-bold text-left ml-8 flex">
         <li className="md:pr-4 md:text-[1.1rem] min-[900px]:text-xl min-[900px]:pr-8  md:hover:scale-[1.2] md:transition-all md:duration-300 md:ease-in-out">
           <NavLink to="/"
-            className={`${location.pathname === "/" ? "text-soil underline" : ""} `}>Home</NavLink>
+            className={`${location.pathname === "/" ? "text-soil underline" : ""} py-4 px-2 `}>Home</NavLink>
         </li>
         <li className="md:pr-4 md:text-[1.1rem] min-[900px]:text-xl min-[900px]:pr-8 md:hover:scale-[1.2] md:transition-all md:duration-300 md:ease-in-out">
           <NavLink to="/about" 
-            className={location.pathname === "/about" ? "text-soil underline" : ""}>About</NavLink>
+            className={`${location.pathname === "/about" ? "text-soil underline" : ""} py-4 px-2 `}>About</NavLink>
         </li >
         <li className="md:pr-4 md:text-[1.1rem] min-[900px]:text-xl min-[900px]:pr-8 md:hover:scale-[1.2] md:transition-all md:duration-300 md:ease-in-out">
           <NavLink to="/projects"          
-            className={location.pathname === "/projects" ? "text-soil underline" : ""}>Projects</NavLink>
+            className={`${location.pathname === "/projects" ? "text-soil underline" : ""} py-4 px-2 `}>Projects</NavLink>
         </li>
         <li className="md:pr-4 md:text-[1.1rem] min-[900px]:text-xl min-[900px]:pr-8 md:hover:scale-[1.2] md:transition-all md:duration-300 md:ease-in-out">
           <NavLink to="/experiences"         
-            className={location.pathname === "/experiences" ? "text-soil underline" : ""}>Experiences</NavLink>
+            className={`${location.pathname === "/experiences" ? "text-soil underline" : ""} py-4 px-2 `}>Experiences</NavLink>
         </li>
         <li className="md:text-[1.1rem] min-[900px]:text-xl md:hover:scale-[1.2] md:transition-all md:duration-300 md:ease-in-out">
           <NavLink to="/connect"      
-            className={location.pathname === "/connect" ? "text-soil underline" : ""}>Connect</NavLink>
+            className={`${location.pathname === "/connect" ? "text-soil underline" : ""} py-4 px-2`}>Connect</NavLink>
         </li>
       </ul>
     </nav>
