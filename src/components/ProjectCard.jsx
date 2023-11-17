3611107f4a7e5951867ae1c52c19c9416d5820e8
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-17 00:37:27 -0800

    Adjusted the border-radius for the project cards on Home page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -25,7 +25,7 @@ const ProjectCard = ({postsData, isOnHome}) => {
                     <figure className="featured-image" dangerouslySetInnerHTML={featuredImage(post._embedded['wp:featuredmedia'][0])}></figure>
                 } */}
                 {post.acf.video_teaser &&
-                  <img className='w-[275px] mx-auto my-2 rounded shadow-md md:!my-0 md:!w-[300px]' src={post.acf.video_teaser} alt="Animated Cartoonized Portrait" />
+                  <img className='w-[275px] mx-auto my-2 rounded-t shadow-md md:!my-0 md:!w-[300px]' src={post.acf.video_teaser} alt="Animated Cartoonized Portrait" />
                 }
                 {post.title.rendered &&
                     <h3 className='font-bold text-[1.3rem] text-center mt-4' dangerouslySetInnerHTML={{ __html: post.title.rendered }} />     
