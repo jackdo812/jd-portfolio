d114c8dbbd972578dd9c5ba03b0245d23ecb3121
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-20 12:24:54 -0800

    place the Loading component in the middle of the page

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 const Loading = () => {
     const isDarkMode = useSelector((state) => state.darkMode);
     return (
-        <div className='absolute top-[15%] left-0 right-0'>
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
             {isDarkMode ?
             <img className="loading block my-0 mx-auto w-[256px] h-[256px] md:w-[512px] md:h-[512px]" src={loadingDarkMode} alt="Loading Animation" id="loading" />
             : 
