682364607bd9fedc1e3bf081f9eb0d89ce7b3f8f
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-19 20:37:23 -0800

    added loading animation for Darkmode

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,16 @@
 import loading from '../assets/gifs/Loading-leaves.gif';
+import loadingDarkMode from '../assets/gifs/Loading_darkmode.gif';
+import { useSelector } from 'react-redux';
 
 const Loading = () => {
+    const isDarkMode = useSelector((state) => state.darkMode);
     return (
         <div className='absolute top-[25%] left-0 right-0'>
-            <img className="loading block my-0 mx-auto w-[256px] h-[256px] md:w-[512px] md:h-[512px]" src={loading} alt="Loading" id="loading" />
+            {isDarkMode ?
+            <img className="loading block my-0 mx-auto w-[256px] h-[256px] md:w-[512px] md:h-[512px]" src={loadingDarkMode} alt="Loading Animation" id="loading" />
+            : 
+            <img className="loading block my-0 mx-auto w-[256px] h-[256px] md:w-[512px] md:h-[512px]" src={loading} alt="Loading Animation" id="loading" />
+            }
         </div>   
     )
 
