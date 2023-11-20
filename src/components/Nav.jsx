fc7d28fc8f7ea3a8b4b31dd0707675d7a177874f
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-19 21:45:56 -0800

    added text-sun for nav currentPage on DarkMode

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,17 @@
 import { NavLink, useLocation } from "react-router-dom";
 import ToggleButton from "./ToggleButton";
+import { useSelector } from 'react-redux';
 
 const Nav = () => {
-
+  const isDarkMode = useSelector((state) => state.darkMode);
   const location = useLocation();
 
   function blur(e) {
     e.target.blur();
   }
+
+  let currentPage = '';
+  isDarkMode ? currentPage = 'text-sun underline' : currentPage = 'text-soil underline';
 
   return (
     <nav className={`main-nav self-center hidden md:block md:justify-evenly`} onClick={blur}>
@@ -15,23 +19,23 @@ const Nav = () => {
       <ul className="font-bold text-left ml-8 flex">
         <li className="md:pr-4 md:text-[1.1rem] min-[900px]:text-xl min-[900px]:pr-8  md:hover:scale-[1.2] md:transition-all md:duration-300 md:ease-in-out">
           <NavLink to="/"
-            className={`${location.pathname === "/" ? "text-soil underline" : ""} py-4 px-2 `}>Home</NavLink>
+            className={`${location.pathname === "/" && currentPage} py-4 px-2 `}>Home</NavLink>
         </li>
         <li className="md:pr-4 md:text-[1.1rem] min-[900px]:text-xl min-[900px]:pr-8 md:hover:scale-[1.2] md:transition-all md:duration-300 md:ease-in-out">
           <NavLink to="/about" 
-            className={`${location.pathname === "/about" ? "text-soil underline" : ""} py-4 px-2 `}>About</NavLink>
+            className={`${location.pathname === "/about" && currentPage} py-4 px-2 `}>About</NavLink>
         </li >
         <li className="md:pr-4 md:text-[1.1rem] min-[900px]:text-xl min-[900px]:pr-8 md:hover:scale-[1.2] md:transition-all md:duration-300 md:ease-in-out">
           <NavLink to="/projects"          
-            className={`${location.pathname === "/projects" ? "text-soil underline" : ""} py-4 px-2 `}>Projects</NavLink>
+            className={`${location.pathname === "/projects" && currentPage} py-4 px-2 `}>Projects</NavLink>
         </li>
         <li className="md:pr-4 md:text-[1.1rem] min-[900px]:text-xl min-[900px]:pr-8 md:hover:scale-[1.2] md:transition-all md:duration-300 md:ease-in-out">
           <NavLink to="/experiences"         
-            className={`${location.pathname === "/experiences" ? "text-soil underline" : ""} py-4 px-2 `}>Experiences</NavLink>
+            className={`${location.pathname === "/experiences" && currentPage} py-4 px-2 `}>Experiences</NavLink>
         </li>
         <li className="md:text-[1.1rem] min-[900px]:text-xl md:hover:scale-[1.2] md:transition-all md:duration-300 md:ease-in-out">
           <NavLink to="/connect"      
-            className={`${location.pathname === "/connect" ? "text-soil underline" : ""} py-4 px-2`}>Connect</NavLink>
+            className={`${location.pathname === "/connect" && currentPage} py-4 px-2`}>Connect</NavLink>
         </li>
         <li>
           <ToggleButton />
