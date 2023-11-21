4505a0e62a19e3dd6d41d6cce0cf3a3044c7fbcd
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-21 14:36:33 -0800

    added styling for NotFoundPage

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -6,8 +6,8 @@ const PageNotFound = () => {
 
   return (
 
-      <section className="page-not-found-section flex flex-col justify-center items-center content-center flex-wrap mt-[7rem] breakpoint-small:mt-[2rem] text-xl">
-        <h2 className="mt-4">404 ... </h2>
+      <section className="page-not-found-section absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-center items-center content-center flex-wrap text-xl md:text-2xl lg:text-3xl">
+        <h2 className="mt-4">404 ... :( </h2>
         <p>Page not found.</p>
         <p>
           Go to <Link to="/" className="underline tablet-b:hover:text-bright-orange transition duration-500">Home</Link> page.
