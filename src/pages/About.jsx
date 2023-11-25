ab4e72d786be978c5d142e09ee0f899d7b2c4a4d
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-24 16:10:40 -0800

    removed loading lazy in some img tags

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -145,31 +145,31 @@ const About = () => {
                         {data.acf.hobby_illustration_desktop &&
                             <div className={`hidden ${isDarkMode && 'md:bg-foggy rounded-lg'} md:block md:relative md:min-h-[650px] md:max-w-[1200px] md:my-0 md:mx-auto min-[800px]:min-h-[700px] min-[900px]:min-h-[800px] min-[1075px]:min-h-[900px] min-[1100px]:min-h-[1000px]`}>
                                 <img className='hidden md:block md:absolute md:bottom-0 ' src={data.acf.hobby_illustration_desktop} alt="3 Mountains Drawing" loading='lazy'/>
-                                <img className={`absolute z-20 w-[65px] left-[52%] bottom-[65%] cursor-pointer`} src={LocationPin} alt="Location Pin GIF" onClick={revealHobby1} loading='lazy'/>
-                                <img className={`absolute z-20 w-[65px] left-[70%] bottom-[35%] cursor-pointer`} src={LocationPin} alt="Location Pin GIF" onClick={revealHobby2} loading='lazy'/>
-                                <img className={`absolute z-20 w-[65px] left-[13.5%] bottom-[49%] cursor-pointer`} src={LocationPin} alt="Location Pin GIF" onClick={revealHobby3} loading='lazy'/>
+                                <img className={`absolute z-20 w-[65px] left-[52%] bottom-[65%] cursor-pointer`} src={LocationPin} alt="Location Pin GIF" onClick={revealHobby1}/>
+                                <img className={`absolute z-20 w-[65px] left-[70%] bottom-[35%] cursor-pointer`} src={LocationPin} alt="Location Pin GIF" onClick={revealHobby2}/>
+                                <img className={`absolute z-20 w-[65px] left-[13.5%] bottom-[49%] cursor-pointer`} src={LocationPin} alt="Location Pin GIF" onClick={revealHobby3}/>
                                 <div className={` ${isHobby1Flipped ? 'block animate-zoom' : 'hidden'} rounded-[10px] shadow-[0_2px_8px_0] shadow-black absolute right-[35%] top-[10%] z-30 w-[200px] min-[950px]:w-[250px] `}>
-                                    <img className={`w-[200px] rounded-[10px] z-30 min-[950px]:w-[250px]`} src={data.acf.hobby_1} alt="Hobby 1" loading='lazy'/>
+                                    <img className={`w-[200px] rounded-[10px] z-30 min-[950px]:w-[250px]`} src={data.acf.hobby_1} alt="Hobby 1" />
                                     <span className={`absolute right-[10px] top-0 z-30 text-[#FF0000] text-2xl cursor-pointer `}>x</span>
                                     <span className={`absolute right-[10px] top-0 z-30 text-white animate-ping text-2xl cursor-pointer`} onClick={revealHobby1}>x</span>
                                     {data.acf.hobby_1_badge &&
-                                        <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_1_badge} alt='Hobby 1 Badge Illustration' loading='lazy'/>
+                                        <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_1_badge} alt='Hobby 1 Badge Illustration'/>
                                      }
                                 </div>
                                 <div className={` ${isHobby2Flipped ? 'block animate-zoom' : 'hidden'} rounded-[10px] shadow-[0_2px_8px_0] shadow-black absolute right-[10%] bottom-[12%] z-30 w-[200px] min-[950px]:w-[250px] min-[950px]:bottom-[8%] min-[950px]:right-[15%] min-[1100px]:bottom-[15%]`}>
-                                    <img className={`w-[200px] rounded-[10px] z-30 min-[950px]:w-[250px]`} src={data.acf.hobby_2} alt="Hobby 2" loading='lazy'/>
+                                    <img className={`w-[200px] rounded-[10px] z-30 min-[950px]:w-[250px]`} src={data.acf.hobby_2} alt="Hobby 2" />
                                     <span className={`absolute right-[10px] top-0 z-30 text-[#FF0000] text-2xl cursor-pointer `}>x</span>
                                     <span className={`absolute right-[10px] top-0 z-30 text-white animate-ping text-2xl cursor-pointer `} onClick={revealHobby2}>x</span>
                                     {data.acf.hobby_2_badge &&
-                                        <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_2_badge} alt='Hobby 2 Badge Illustration' loading='lazy'/>
+                                        <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_2_badge} alt='Hobby 2 Badge Illustration' />
                                      }
                                 </div>
                                 <div className={` ${isHobby3Flipped ? 'block animate-zoom' : 'hidden'} rounded-[10px] shadow-[0_2px_8px_0] shadow-black absolute left-[5%] bottom-[25%] z-30 w-[200px] min-[950px]:w-[250px] min-[950px]:bottom-[25%] min-[950px]:left-[5%] min-[1100px]:bottom-[30%] min-[1100px]:left-[10%]`}>
-                                    <img className={`w-[200px] rounded-[10px] z-30 min-[950px]:w-[250px]`} src={data.acf.hobby_3} alt="Hobby 2" loading='lazy'/>
+                                    <img className={`w-[200px] rounded-[10px] z-30 min-[950px]:w-[250px]`} src={data.acf.hobby_3} alt="Hobby 2" />
                                     <span className={`absolute right-[10px] top-0 z-30 text-[#FF0000] text-2xl cursor-pointer `}>x</span>
                                     <span className={`absolute right-[10px] top-0 z-30 text-white animate-ping text-2xl cursor-pointer `} onClick={revealHobby3}>x</span>
                                     {data.acf.hobby_3_badge &&
-                                        <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_3_badge} alt='Hobby 3 Badge Illustration' loading='lazy'/>
+                                        <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_3_badge} alt='Hobby 3 Badge Illustration' />
                                      }
                                 </div>
                             </div>
@@ -183,17 +183,17 @@ const About = () => {
                                     <div className="front-component">
                                             <div className='flip-card-front mx-auto my-0 rounded-[10px] max-w-[250px] backface-hidden z-20 h-[300px] border relative bg-white shadow-[0_2px_8px_0] shadow-black'>
                                                 <img className={`absolute w-[250px] z-10 left-0 bottom-0`} src={data.acf.hobby_1_illustration} alt="Illustration for Hobby 1" loading='lazy'/>
-                                                <img className={`absolute z-20 w-[70px] left-[45%] bottom-[34%] cursor-pointer`} src={LocationPinLetter} alt="Location Pin GIF" onClick={revealHobby1} loading='lazy'/>
+                                                <img className={`absolute z-20 w-[70px] left-[45%] bottom-[34%] cursor-pointer`} src={LocationPinLetter} alt="Location Pin GIF" onClick={revealHobby1} />
                                             </div>
                                     </div>
 
                                     <div className="back-component">
                                             <div className={`flip-card-back mx-auto my-0 rounded-[10px] w-[240px] transform rotate-y-180 backface-hidden relative shadow-[0_2px_8px_0] shadow-black `}>
-                                                <img className={`w-[240px] rounded-[10px]`} src={data.acf.hobby_1} alt="Hobby 1" loading='lazy'/>
+                                                <img className={`w-[240px] rounded-[10px]`} src={data.acf.hobby_1} alt="Hobby 1" />
                                                 <span className={`absolute right-[10px] top-0 z-20 text-[#FF0000] text-2xl cursor-pointer`}>x</span>
                                                 <span className={`absolute right-[10px] top-0 z-20 text-white animate-ping text-2xl cursor-pointer`} onClick={revealHobby1}>x</span>
                                                 {data.acf.hobby_1_badge &&
-                                                    <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_1_badge} alt='Hobby 1 Badge Illustration' loading='lazy'/>
+                                                    <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_1_badge} alt='Hobby 1 Badge Illustration' />
                                                 }
                                             </div>
                                     </div>
@@ -209,17 +209,17 @@ const About = () => {
                                     <div className="front-component">
                                             <div className='flip-card-front mx-auto my-0 rounded-[10px] max-w-[250px] backface-hidden z-20 h-[300px] border relative bg-white shadow-[0_2px_8px_0] shadow-black'>
                                                 <img className={`absolute w-[250px] z-10 left-0 bottom-0`} src={data.acf.hobby_2_illustration} alt="Illustration for Hobby 1" loading='lazy'/>
-                                                <img className={`absolute z-20 w-[70px] left-[38%] bottom-[26%] cursor-pointer`} src={LocationPinLetter} alt="Location Pin GIF" onClick={revealHobby2} loading='lazy'/>
+                                                <img className={`absolute z-20 w-[70px] left-[38%] bottom-[26%] cursor-pointer`} src={LocationPinLetter} alt="Location Pin GIF" onClick={revealHobby2} />
                                             </div>
                                     </div>
 
                                     <div className="back-component">
                                             <div className={`flip-card-back mx-auto my-0 rounded-[10px] w-[240px] transform rotate-y-180 backface-hidden relative shadow-[0_2px_8px_0] shadow-black`}>
-                                                <img className={`w-[240px] rounded-[10px]`} src={data.acf.hobby_2} alt="Hobby 1" loading='lazy'/>
+                                                <img className={`w-[240px] rounded-[10px]`} src={data.acf.hobby_2} alt="Hobby 1" />
                                                 <span className={`absolute right-[10px] top-0 z-20 text-[#FF0000] text-2xl cursor-pointer`}>x</span>
                                                 <span className={`absolute right-[10px] top-0 z-20 text-white animate-ping text-2xl cursor-pointer`} onClick={revealHobby2}>x</span>
                                                 {data.acf.hobby_2_badge &&
-                                                    <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_2_badge} alt='Hobby 2 Badge Illustration' loading='lazy'/>
+                                                    <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_2_badge} alt='Hobby 2 Badge Illustration' />
                                                 }
                                             </div>
                                     </div>
@@ -233,17 +233,17 @@ const About = () => {
                                     <div className="front-component">
                                             <div className='flip-card-front mx-auto my-0 rounded-[10px] max-w-[250px] backface-hidden z-20 h-[320px] border relative bg-white shadow-[0_2px_8px_0] shadow-black'>
                                                 <img className={`absolute w-[250px] z-10 left-0 bottom-0`} src={data.acf.hobby_3_illustration} alt="Illustration for Hobby 1" loading='lazy'/>
-                                                <img className={`absolute z-20 w-[70px] left-[58%] bottom-[17%] cursor-pointer`} src={LocationPinLetter} alt="Location Pin GIF" onClick={revealHobby3} loading='lazy'/>
+                                                <img className={`absolute z-20 w-[70px] left-[58%] bottom-[17%] cursor-pointer`} src={LocationPinLetter} alt="Location Pin GIF" onClick={revealHobby3}/>
                                             </div>
                                     </div>
 
                                     <div className="back-component">
                                             <div className={`flip-card-back mx-auto my-0 rounded-[10px] w-[240px] transform rotate-y-180 backface-hidden relative shadow-[0_2px_8px_0] shadow-black`}>
-                                                <img className={`w-[240px] rounded-[10px]`} src={data.acf.hobby_3} alt="Hobby 1" loading='lazy'/>
+                                                <img className={`w-[240px] rounded-[10px]`} src={data.acf.hobby_3} alt="Hobby 1" />
                                                 <span className={`absolute right-[10px] top-0 z-20 text-[#FF0000] text-2xl cursor-pointer`}>x</span>
                                                 <span className={`absolute right-[10px] top-0 z-20 text-white animate-ping text-2xl cursor-pointer`} onClick={revealHobby3}>x</span>
                                                 {data.acf.hobby_3_badge &&
-                                                    <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_3_badge} alt='Hobby 3 Badge Illustration' loading='lazy'/>
+                                                    <img className={`absolute w-[50px] left-[10px] top-0 z-20`} src={data.acf.hobby_3_badge} alt='Hobby 3 Badge Illustration' />
                                                 }
                                             </div>
                                     </div>
