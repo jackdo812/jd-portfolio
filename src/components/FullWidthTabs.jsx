4b529d649fec325370bd6c4c958e58a952951e81
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-12-13 21:51:52 -0800

    added styles to ul li to single project with single tab

--- src/components/FullWidthTabs.jsx
+++ src/components/FullWidthTabs.jsx
@@ -13,7 +13,7 @@ function FullWidthTabs ({dataACF}) {
           <TabList className={`text-foggy font-bold rounded-t-[10px] bg-leaf flex text-center ${listProjectsDetailsLength === 3 && 'text-[0.8rem] min-[400px]:text-[1rem] md:text-[1.1rem]'}`}> 
             {listProjectsDetails.map((item, index) => {
                 return (
-                    <Tab className={`${listProjectsDetailsLength === 2 ? 'w-[50%]' : 'w-[33.3%]'} cursor-pointer p-2`} selectedClassName={`${listProjectsDetailsLength === 2 ? 'w-[50%]' : 'w-[33.4%]'} bg-foggy text-forest font-bold underline rounded-t-[7px] decoration-soil decoration-4 underline-offset-4`} key={index}>{item.single_project_details_heading}</Tab>
+                    <Tab className={`${listProjectsDetailsLength === 2 ? 'w-[50%] text-[1.1rem]' : 'w-[33.3%]'} cursor-pointer p-2`} selectedClassName={`${listProjectsDetailsLength === 2 ? 'w-[50%]' : 'w-[33.4%]'} bg-foggy text-forest font-bold underline rounded-t-[7px] decoration-soil decoration-4 underline-offset-4`} key={index}>{item.single_project_details_heading}</Tab>
                 );
             })}
           </TabList>
@@ -33,7 +33,7 @@ function FullWidthTabs ({dataACF}) {
         // If the Project Details list is 1, then display the content without the Tabs layout
         listProjectsDetails.map((item, index) => {
             return (
-              <div className='bg-foggy rounded mt-4 shadow-[0_0_4px_1px_gray] py-2'>
+              <div key={index} className='bg-foggy rounded mt-4 shadow-[0_0_4px_1px_gray] py-2'>
                 <Fade>
                   <h3 className='font-bold text-[1.2rem] pt-1 pb-3 text-forest my-2 mx-4 border-b-2'>{item.single_project_details_heading}</h3>
                  <div className='single-project-details-content text-forest my-2 mx-4 ' dangerouslySetInnerHTML={{__html:item.single_project_details_content}}></div>
